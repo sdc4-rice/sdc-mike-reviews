4e7667bf9ed85e83d430491eac4bd706352861e1
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-05 16:30:32 -0700

    set up express server skeleton

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.get('/', (req, res) => res.send('YUP'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
