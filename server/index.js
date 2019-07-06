8d30a190645eea7079f5ebc9850eb9119e824f34
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-05 19:44:45 -0700

    get to  /reviews responds with data from database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3002;
-const {db, Review} = require('./db');
+const {db, Reviews} = require('./db');
 
 app.get('/', (req, res) => res.send('YUP'));
+
+app.get('/reviews', (req, res) => {
+  Reviews.find()
+    .then((data) => res.send(data))
+    .catch((err) => res.send(err));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
