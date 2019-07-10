e8b3d9f79be4165df06ffb3183ae4dc78a503d7c
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-10 03:09:55 -0700

    commented out db connected log

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('connected to DB'));
+// db.once('open', () => console.log('connected to DB')); //recieving error code in Jest. Not sure if its even needed
 
 let reviewSchema = new mongoose.Schema({
   author: String,
