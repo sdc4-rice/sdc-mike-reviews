224413deef8c619f6cc2d014957870fa53d9c3da
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-11 00:50:57 -0700

    attempt to run test on seedDB function

--- test/seeder.test.js
+++ test/seeder.test.js
@@ -1,4 +1,5 @@
-const {makeReview} = require ('../server/seeder.js');
+const {makeReview, seedDB} = require ('../server/seeder.js');
+const { db, Reviews } = require('../server/db.js');
 
 describe('Review Maker', () => {
   var review = makeReview();
@@ -26,3 +27,21 @@ describe('Review Maker', () => {
   });
 
 });
+
+// describe('seedDB', () => {
+
+//   var documents;
+
+//   beforeAll(async () => {
+//     await seedDB();
+//     await db.Reviews.find()
+//       .then((reviews) => {
+//         documents = reviews;
+//       });
+//   });
+
+//   test('DB should have 100 documents', () => {
+//     expect(documents.length).toBe(100);
+//   });
+
+// });
