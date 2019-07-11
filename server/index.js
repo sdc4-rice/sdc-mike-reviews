1ff85c2b532e17c9626794c5357beeca81240f5b
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-10 19:52:55 -0700

    Build post /reviews

--- server/index.js
+++ server/index.js
@@ -11,4 +11,37 @@ app.get('/reviews', (req, res) => {
     .catch((err) => res.status(500).send('Error on our side'));
 });
 
+app.post('/reviews', (req, res) => {
+  var newReview = new Reviews(req.body);
+  newReview.save((error, document, rows) => {
+    if (error) {
+      res.status(500).send('Error on the serverside');
+    } else {
+      res.send(document); //responding back with document that will be added to the state for re render.
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+const express = require('express');
+const app = express();
+const port = 3002;
+const bodyParser = require('body-parser');
+const {db, Reviews} = require('./db');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('YUP'));//TODO: server static files
+
+app.get('/reviews', (req, res) => {
+  Reviews.find()
+    .then((data) => res.send(data))
+    .catch((err) => res.status(500).send('Error on our side'));
+});
+
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
