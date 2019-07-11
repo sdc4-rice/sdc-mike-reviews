cca73882a05fe57ddc0892b9a6402a4b372de760
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-11 16:44:23 -0700

    built test for post and get

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,43 @@
 const request = require('supertest');
-const express = require('express');
+const { seedDB, makeReview } = require('../server/seeder.js');
 
-const app = express();
+describe('GET /reviews', () => {
+
+  test('Responds with json', (done) => {
+    request('http://localhost:3002')
+      .get('/reviews')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+
+  test('Responds with array of objects', (done) => {
+    request('http://localhost:3002')
+      .get('/reviews')
+      .expect(200)
+      .then((result) => {
+        expect(Array.isArray(result.body)).toBe(true);
+        expect(typeof result.body[0]).toBe('object');
+        done();
+      });
+  });
+});
+
+describe('POST /reviews', () => {
+
+  test('Post responds with updated review', (done) => {
+    let review = makeReview();
+    request('http://localhost:3002')
+      .post('/reviews')
+      .send(review)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((response) => {
+        expect(response.body.author).toEqual(review.author);
+        expect(response.body.rating).toEqual(review.rating);
+        expect(response.body.popularity).toEqual(review.popularity);
+        expect(response.body.review).toEqual(review.review);
+        done();
+      });
+  });
+});
