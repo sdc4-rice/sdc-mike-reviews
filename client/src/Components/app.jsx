b4dbac4be9b4f240ed1e1989c795df1e6a28d77d
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-12 01:28:42 -0700

    Created a Componets directory with app.jsx, App class component

---
+++ client/src/Components/app.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <div id="header">
+
+        </div>
+        <div id="ratings">
+
+        </div>
+        <div id="reviews">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
