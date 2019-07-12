bc4b209634b040d9444b84f8a9cd2d04fd7df85e
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-12 01:29:24 -0700

    rendering App

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './Components/app.jsx';
 
-ReactDOM.render(<h1>TEST?</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
