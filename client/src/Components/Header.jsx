3fe0bf64fb46ac28718a09cd5d3a60c29b25c252
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-14 22:52:03 -0700

    renders header title and button. No styling

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+var Header = function () {
+  return (
+    <div>
+      <h1>Ratings & Reviews</h1>
+      <button>Write a review</button>
+    </div>
+  );
+};
+
+export default Header;
