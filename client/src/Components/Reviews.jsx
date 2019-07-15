2451a2a64c250184eb6feee082ea6af8b4598c6a
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-15 16:24:34 -0700

    renders Reviews class component with mock data

---
+++ client/src/Components/Reviews.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Reviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div id="reviews">
+        <h1>RENDER REVIEWS HERE</h1>
+      </div>
+    );
+  }
+}
+
+export default Reviews;
