6d159fbf0cb4a2279451288d63d5bd1fd087c538
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-16 11:43:37 -0700

    urlencoded to true

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const port = 3002;
 const {db, Reviews} = require('./db');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
