604ec494a7a22073c02b8787e40bc05a2f6539c9
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-16 15:16:29 -0700

    rendering reviews and sub components

--- client/src/Components/Reviews.jsx
+++ client/src/Components/Reviews.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 
 class Reviews extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
 
-    };
-  }
+  //   };
+  // }
 
   render() {
     return (
       <div id="reviews">
-        <h1>RENDER REVIEWS HERE</h1>
+        <div className="review">
+          <h3>Most relevant reviews</h3>
+          {this.props.reviews.map(review =>
+            <div>
+              <div id="users">
+                <div id="users_rating">{review.rating}</div>
+                <div id="users_author">{review.author}</div>
+                <div id="users_date">{review.date}</div>
+              </div>
+              <div className="reviews">
+                <div id="review_title">{review.review.title}</div>
+                <div id="review">{review.review.review}</div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
