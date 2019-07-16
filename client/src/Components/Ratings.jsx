04a9056b8bb07d1663c708a20ccfe2a1a034f547
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-16 13:16:38 -0700

    renders rating average and ratings count

--- client/src/Components/Ratings.jsx
+++ client/src/Components/Ratings.jsx
@@ -2,69 +2,58 @@ import React from 'react';
 import style from '../Styles/ratings.css';
 
 class Ratings extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
 
-    };
+  calculateRating() {
+    let total = 0;
+    for (let i = 0; i < this.props.reviews.length; i++) {
+      let rating = this.props.reviews[i].rating;
+      total += rating;
+    }
+    total = total / (this.props.reviews.length + 1);
+    return Math.floor(total);
   }
 
   render() {
     return (
       <div id="ratings">
-        <span id="number_rating">
-          <h1 id="number">5.0</h1>
-          <div id="stars">*****</div>
-          <div id="count">X product ratings</div>
-        </span>
-        <span id="rating_bars">
-          <div id="bar_one">
-            * 5 ----------- 0
+        <div id="ratings_box">
+          <div id="number_rating">
+            <h1 id="number">{this.calculateRating()}</h1>
+            <div id="stars">*****</div>
+            <div id="count">{this.props.reviews.length} product ratings</div>
           </div>
-          <div id="bar_one">
-            * 4 ----------- 0
+          <div id="rating_bars">
+            <div id="bar_one">
+              * 5 ----------- 0
+            </div>
+            <div id="bar_one">
+              * 4 ----------- 0
+            </div>
+            <div id="bar_one">
+              * 3 ----------- 0
+            </div>
+            <div id="bar_one">
+              * 2 ----------- 0
+            </div>
+            <div id="bar_one">
+              * 1 ----------- 0
+            </div>
           </div>
-          <div id="bar_one">
-            * 3 ----------- 0
+          <div id="rating_charts">
+            <div id="chart1">
+              ()<div id="category">Would recommend</div>
+            </div>
+            <div id="chart2">
+              ()<div id="category">Good quality</div>
+            </div>
+            <div id="chart3">
+              ()<div id="category">Great value</div>
+            </div>
           </div>
-          <div id="bar_one">
-            * 2 ----------- 0
-          </div>
-          <div id="bar_one">
-            * 1 ----------- 0
-          </div>
-        </span>
-        <span id="rating_charts">
-          <div id="chart1">
-            ()<div id="category">Would recommend</div>
-          </div>
-          <div id="chart2">
-            ()<div id="category">Good quality</div>
-          </div>
-          <div id="chart3">
-            ()<div id="category">Great value</div>
-          </div>
-        </span>
+        </div>
       </div>
     );
   }
 }
 
 export default Ratings;
-
-// Number rating
-//number
-//stars
-//ratings count
-
-//Rating bars
-//Rating bar 5
-//Rating bar 4
-//Rating bar 3
-//Rating bar 2
-//Rating bar 1
-
-//Ratings charts
-//percentage chart
-//percentage chart
-//percentage chart
