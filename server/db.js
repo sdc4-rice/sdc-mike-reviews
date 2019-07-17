3a35497e1e4ffcfd75a800c2f7505c07c3222724
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-17 02:35:53 -0700

    added productId

--- server/db.js
+++ server/db.js
@@ -8,6 +8,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 // db.once('open', () => console.log('connected to DB')); //recieving error code in Jest. Not sure if its even needed
 
 let reviewSchema = new mongoose.Schema({
+  productId: Number,
   author: String,
   rating: Number,
   date: Date,
