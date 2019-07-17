00f1cb94980375bc1c761b57f7196e48bc45091b
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-17 13:13:11 -0700

    changed id root to ratings_reviews

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/app.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('ratings_reviews'));
