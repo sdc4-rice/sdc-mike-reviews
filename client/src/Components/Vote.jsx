16f4ab26ca35f14ac127f3baa8050e11872283f1
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-20 16:35:45 -0700

    clean up styling

--- client/src/Components/Vote.jsx
+++ client/src/Components/Vote.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Vote (props) {
   return (
     <div id="vote_buttons">
-                    <button id="upvote" onClick={props.vote} className={props._id} name="upvote">👍</button>
-                    <button id="downvote" onClick={props.vote} className={props._id} name="downvote">👎</button>
-                  </div>
+      <button id="upvote" onClick={props.vote} className={props._id} name="upvote">👍</button>
+      <button id="downvote" onClick={props.vote} className={props._id} name="downvote">👎</button>
+    </div>
   )
 }
 
