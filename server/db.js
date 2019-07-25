9c4862e21672fadcedb6178bf8fe6522b0f97daa
Mike Chung <m.chung1128@gmail.com>
2019-07-25 13:22:49 -0400

    Comment out Mongo schema and replace with PostGreSQL/Sequelize

--- server/db.js
+++ server/db.js
@@ -1,23 +1,55 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true, useFindAndModify: true });
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true, useFindAndModify: true });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+// db.on('error', console.error.bind(console, 'connection error:'));
 
-let reviewSchema = new mongoose.Schema({
-  productId: Number,
-  author: String,
-  rating: Number,
-  date: Date,
-  review: {
-    title: String,
-    review: String },
-  popularity: Number
+// let reviewSchema = new mongoose.Schema({
+//   productId: Number,
+//   author: String,
+//   rating: Number,
+//   date: Date,
+//   review: {
+//     title: String,
+//     review: String },
+//   popularity: Number
+// });
+
+// let Reviews = mongoose.model('Reviews', reviewSchema);
+
+// module.exports = {
+//   db, Reviews
+// };
+
+const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('postgres://postgres@localhost:5432/reviews');
+const sequelize = new Sequelize('reviews', 'postgres', '', {
+  host: 'localhost',
+  dialect: 'postgres'
 });
 
-let Reviews = mongoose.model('Reviews', reviewSchema);
+class Review extends Sequelize.Model{}
+
+Review.init({
+  productId: Sequelize.INTEGER,
+  author: Sequelize.STRING,
+  rating: Sequelize.INTEGER,
+  date: Sequelize.DATEONLY,
+  review: Sequelize.JSON,
+  popularity: Sequelize.INTEGER
+}, { sequelize, modelName: 'Review' });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connected to db!')
+  })
+  .catch(err => {
+    console.log('Unable to connect to db:', err);
+  });
 
 module.exports = {
-  db, Reviews
+  sequelize,
+  Review
 };
