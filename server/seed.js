7cfe19d90e95d5e1899685d47014b90189bf60e9
Mike Chung <m.chung1128@gmail.com>
2019-07-25 16:37:01 -0400

    Add console log to know when seeding is complete

--- server/seed.js
+++ server/seed.js
@@ -1,4 +1,4 @@
 const { seedDB } = require('./seeder.js');
 const { Review } = require('./db.js');
 
-Review.sync({ force: true }).then(() => seedDB());
+Review.sync({ force: true }).then(() => seedDB(1, 1000000)).then(() => console.log('Done seeding!'));
