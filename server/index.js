a7fe7100567325df6fb882139e0bed3512833862
Mike Chung <m.chung1128@gmail.com>
2019-07-25 20:43:09 -0400

    Implement API routes with model functions for PostGreSQL/Sequelize

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const {db, Reviews} = require('./db');
+const { Review } = require('./db');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
@@ -13,29 +13,74 @@ app.use(cors());
 
 app.use(express.static('public'));
 
+// app.get('/reviews/:id', (req, res) => {
+//   const productId = req.params.id;
+//   Reviews.find({productId: productId})
+//     .then((data) => res.send(data))
+//     .catch((err) => res.status(500).send('Error on our side'));
+// });
+
+// app.post('/reviews', (req, res) => {
+//   let newReview = new Reviews(req.body);
+//   newReview.save((error, document, rows) => {
+//     if (error) {
+//       res.status(500).send('Error on the serverside');
+//     } else {
+//       res.send(document); //responding back with document that will be added to the state for re render.
+//     }
+//   });
+// });
+
+// app.put('/reviews', (req, res) => {
+//   let query = {_id: req.body._id};
+//   let vote = req.body.vote;
+
+//   Reviews.findOne(query)
+//     .then((review) => Number(review.popularity))
+//     .then((popularity) => {
+//       if (vote === 'upvote') {
+//         popularity++;
+//       } else if (vote === 'downvote') {
+//         popularity--;
+//       }
+//       return popularity;
+//     })
+//     .then((newPopularity) => Reviews.findOneAndUpdate(query, {$set: {popularity: newPopularity}}, {new: true}))
+//     .then((updatedReview) => {
+//       res.send(updatedReview);
+//       return updatedReview;
+//     })
+//     .catch(err => console.log('there was an err finding in db'));
+// });
+
+// app.delete('/delete/reviews',(req, res) => {
+//   let query = {_id: req.body._id};
+//   Reviews.remove(query)
+//     .then(data => res.send('Successfully deleted!'))
+//     .catch(err => console.log('Error deleting review:', err))
+// });
+
 app.get('/reviews/:id', (req, res) => {
   const productId = req.params.id;
-  Reviews.find({productId: productId})
-    .then((data) => res.send(data))
-    .catch((err) => res.status(500).send('Error on our side'));
+  Review.findAll({
+    where: {
+      productId: productId
+    }
+  })
+    .then((data) => res.status(200).send(data).end())
+    .catch((err) => console.log('Error getting reviews!'))
 });
 
 app.post('/reviews', (req, res) => {
-  let newReview = new Reviews(req.body);
-  newReview.save((error, document, rows) => {
-    if (error) {
-      res.status(500).send('Error on the serverside');
-    } else {
-      res.send(document); //responding back with document that will be added to the state for re render.
-    }
-  });
+  Review.create(req.body)
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.send('Error posting!'));
 });
 
 app.put('/reviews', (req, res) => {
-  let query = {_id: req.body._id};
+  let query = {id: req.body.id};
   let vote = req.body.vote;
-
-  Reviews.findOne(query)
+  Review.findOne(query)
     .then((review) => Number(review.popularity))
     .then((popularity) => {
       if (vote === 'upvote') {
@@ -45,7 +90,7 @@ app.put('/reviews', (req, res) => {
       }
       return popularity;
     })
-    .then((newPopularity) => Reviews.findOneAndUpdate(query, {$set: {popularity: newPopularity}}, {new: true}))
+    .then((newPopularity) => Review.update(query, {$set: {popularity: newPopularity}}, {new: true}))
     .then((updatedReview) => {
       res.send(updatedReview);
       return updatedReview;
@@ -53,11 +98,15 @@ app.put('/reviews', (req, res) => {
     .catch(err => console.log('there was an err finding in db'));
 });
 
-app.delete('/delete/reviews',(req, res) => {
-  let query = {_id: req.body._id};
-  Reviews.remove(query)
-    .then(data => res.send('Successfully deleted!'))
-    .catch(err => console.log('Error deleting review:', err))
+app.delete('/reviews/:id', (req, res) => {
+  let id = req.params.id;
+  Review.destroy({
+    where: {
+      id: id
+    }
+  })
+    .then(data => res.sendStatus(200).send(data))
+    .catch(err => console.log('Error deleting: ', err))
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
