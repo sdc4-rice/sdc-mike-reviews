c86d962e40634a02ecdc804917b2a7908adb0a01
Mike Chung <m.chung1128@gmail.com>
2019-07-26 15:16:18 -0400

    Change seed number to 10 mil

--- server/seed.js
+++ server/seed.js
@@ -1,4 +1,10 @@
 const { seedDB } = require('./seeder.js');
 const { Review } = require('./db.js');
 
-Review.sync({ force: true }).then(() => seedDB(1, 1000000)).then(() => console.log('Done seeding!'));
+Review.sync({ force: true })
+  .then(() => {
+    seedDB(1, 10000000);
+  })
+  .then(() => {
+    console.log('Done seeding!');
+  });
