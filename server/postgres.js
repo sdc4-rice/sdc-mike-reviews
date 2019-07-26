3f59cab0c0b568f3850e25fbf695c6328ba71541
Mike Chung <m.chung1128@gmail.com>
2019-07-26 19:12:07 -0400

    Move PostgreSQL db into new file

---
+++ server/postgres.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('postgres://postgres@localhost:5432/reviews');
+const sequelize = new Sequelize('reviews', 'postgres', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+  logging: false
+});
+
+class Review extends Sequelize.Model{}
+
+Review.init({
+  productid: Sequelize.INTEGER,
+  author: Sequelize.STRING,
+  rating: Sequelize.INTEGER,
+  date: Sequelize.DATEONLY,
+  review: Sequelize.JSON,
+  popularity: Sequelize.INTEGER
+}, { sequelize, modelName: 'review' });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connected to db!')
+  })
+  .catch(err => {
+    console.log('Unable to connect to db:', err);
+  });
+
+module.exports = {
+  sequelize,
+  Review
+};
