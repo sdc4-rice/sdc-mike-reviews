fd7b7fcd9417cff68fda8cb288aa8e99916a0fac
Mike Chung <m.chung1128@gmail.com>
2019-07-30 17:10:09 -0400

    Move functions around

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config();
 const currentDB = process.env.DB;
 const db = require(`./${currentDB}.js`);
 const bodyParser = require('body-parser');
@@ -13,6 +13,49 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.static('public'));
+
+app.get('/reviews/:id', (req, res) => {
+  const productid = req.params.id;
+  db.getReviews(productid)
+    .then((data) => res.status(200).send(data).end())
+    .catch((err) => console.log('Error getting reviews: ', err))
+});
+
+app.get('/reviews', (req, res) => {
+  db.getAllReviews()
+    .then(data => res.send(data))
+    .catch(err => console.log('Error getting reviews: ', err))
+})
+
+app.post('/reviews', (req, res) => {
+  if (currentDB === 'cassandra') {
+    db.postReview(JSON.stringify(req.body))
+      .then((data) => res.status(200).send(data))
+      .catch((err) => res.send('Error posting: ', err));
+  } else if (currentDB === 'postgres') {
+    db.postReview(req.body)
+      .then((data) => res.status(200).send(data))
+      .catch((err) => res.send('Error posting: ', err));
+  }
+});
+
+app.put('/reviews', (req, res) => {
+  let query = {id: req.body.id};
+  let vote = req.body.vote;
+  db.updateReview(query, vote)
+    .then((updatedReview) => {
+      res.send(updatedReview);
+      return updatedReview;
+    })
+    .catch(err => console.log('there was an err finding in db'));
+});
+
+app.delete('/delete/reviews',(req, res) => {
+  let query = {_id: req.body._id};
+  db.deleteReview(query)
+    .then(data => res.send('Successfully deleted!'))
+    .catch(err => console.log('Error deleting review:', err))
+});
 
 // app.get('/reviews/:id', (req, res) => {
 //   const productId = req.params.id;
@@ -60,36 +103,5 @@ app.use(express.static('public'));
 //     .then(data => res.send('Successfully deleted!'))
 //     .catch(err => console.log('Error deleting review:', err))
 // });
-
-app.get('/reviews/:id', (req, res) => {
-  const productid = req.params.id;
-  db.getReviews(productid)
-    .then((data) => res.status(200).send(data).end())
-    .catch((err) => console.log('Error getting reviews!'))
-});
-
-app.post('/reviews', (req, res) => {
-  db.postReview(req.body)
-    .then((data) => res.status(200).send(data))
-    .catch((err) => res.send('Error posting!'));
-});
-
-app.put('/reviews', (req, res) => {
-  let query = {id: req.body.id};
-  let vote = req.body.vote;
-  db.updateReview(query, vote)
-    .then((updatedReview) => {
-      res.send(updatedReview);
-      return updatedReview;
-    })
-    .catch(err => console.log('there was an err finding in db'));
-});
-
-app.delete('/delete/reviews',(req, res) => {
-  let query = {_id: req.body._id};
-  db.deleteReview(query)
-    .then(data => res.send('Successfully deleted!'))
-    .catch(err => console.log('Error deleting review:', err))
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
